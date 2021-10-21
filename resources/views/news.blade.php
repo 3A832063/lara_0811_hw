98c6b0740c20b03a31a4480520e847fccbf913e3
LIN GUAN-YU <kib7410@gmail.com>
2021-10-21 20:04:05 +0800

    '建立2個連結至另外2個view的按鈕'

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>最新消息</title>
+    <button onclick="location.href='{{route('home.index')}}'">首頁</button>
+    <button onclick="location.href='{{route('about.index')}}'">關於本站</button>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
